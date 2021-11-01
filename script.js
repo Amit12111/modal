9234854690bacec6a89919f192054dade04159fb
Amit12111 <78673730+Amit12111@users.noreply.github.com>
2021-11-01 13:29:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,33 @@
 let close = document.querySelector(".closebtn");
 let open = document.querySelector(".modalbtn1");
 let modal = document.querySelector(".modal1");
-close.addEventListener("click", function () {
-  modal.classList.remove("in");
-  modal.classList.add("back");
-  modal.style.display = `none`;
-});
+// close.addEventListener("click", function () {
+//   modal.classList.remove("in");
+//   modal.classList.add("back");
+//   modal.style.display = `none`;
+// });
 open.addEventListener("click", function () {
   modal.classList.remove("back");
   modal.classList.add("in");
   modal.style.display = `block`;
 });
-window.addEventListener("click", function (e) {
+document.addEventListener("click", function (e) {
   if (e.target === modal) {
     modal.style.display = `none`;
   }
+});
+
+window.addEventListener("keydown", function (e) {
+  if (e.key === "Enter" && modal.style.display ===`block`) {
+    modal.style.display = `none`;
+  }
+});
+
+//  ewainy's code
+close.addEventListener("click", function () {
+  modal.classList.add('effect');
+  setTimeout(()=>{ 
+    modal.style.display = 'none';
+    modal.classList.remove('effect');
+  },1000)
 });
