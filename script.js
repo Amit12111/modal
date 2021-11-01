b99e174be8313410fb5df0849ccc46edeef3f0db
Amit12111 <78673730+Amit12111@users.noreply.github.com>
2021-11-01 10:19:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ let close = document.querySelector(".closebtn");
 let open = document.querySelector(".modalbtn1");
 let modal = document.querySelector(".modal1");
 close.addEventListener("click", function () {
+  modal.classList.remove("in");
+  modal.classList.add("back");
   modal.style.display = `none`;
 });
 open.addEventListener("click", function () {
+  modal.classList.remove("back");
+  modal.classList.add("in");
   modal.style.display = `block`;
 });
 window.addEventListener("click", function (e) {
